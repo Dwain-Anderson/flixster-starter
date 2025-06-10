e82dc3c2d0abdf684bbe75f51707027a0395cdae
Dwain Kyle Anderson <dwainanderson@meta.com>
2025-06-09 19:11:04 -0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import './App.css'
 
 const App = () => {
   return (
-    <div className="App">
-    
-    </div>
+    <>
+    <Header></Header>
+    <p>Initial commit</p>
+    <Footer></Footer>
+    </>
   )
 }
 
